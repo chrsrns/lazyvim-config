83baff132c8e2247277cb2a2077e4243d4f4180c
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-17 10:35:31 +0800

    Added some treesitter tooling

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -9,4 +9,29 @@ return {
       })
     end,
   },
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "comment",
+          "diff",
+          "dockerfile",
+          "dot",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "graphql",
+          "hcl",
+          "http",
+          "jq",
+          "make",
+          "mermaid",
+          "sql",
+        })
+      end
+    end,
+  },
 }
