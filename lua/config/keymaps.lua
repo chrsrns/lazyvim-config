e651a45f55ae99493eb7905a1af7798e5c69d4b2
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-17 11:03:49 +0800

    Prevent delete key from cutting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Prevent the 'd' key from cutting to clipboard
+vim.keymap.set({ "n", "x" }, "d", '"_d', { desc = "Delete", remap = false })
