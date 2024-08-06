0caf80f0614f5334e25f29e88911f3ec6ad2a3bc
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-06 20:58:16 +0800

    Added config for webc

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- WebC configuration
+vim.filetype.add({
+  extension = {
+    webc = "html",
+  },
+  filename = { [".webc"] = "html" },
+  filetype = { [".webc"] = "html" },
+})
+vim.treesitter.language.register("html", "webc")
